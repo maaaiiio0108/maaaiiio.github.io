9151d99e08c5cef19a7217f20d8712af83fcc284
maaaiiio <maaaiiio@me.com>
2017-11-16 03:56:54 +0900

    forGit

--- service-worker.js
+++ service-worker.js
@@ -5,19 +5,19 @@ var filesToCache = [
     '/index.html',
     '/scripts/app.js',
     '/styles/inline.css',
-    'images/clear.png',
-    'images/cloudy-scattered-showers.png',
-    'images/cloudy.png',
-    'images/fog.png',
-    'images/ic_add_white_24px.svg',
-    'images/ic_refresh_white_24px.svg',
-    'images/partly-cloudy.png',
-    'images/rain.png',
-    'images/scattered-showers.png',
-    'images/sleet.png',
-    'images/snow.png',
-    'images/thunderstorm.png',
-    'images/wind.png'
+    '/maaaiiio.github.io/images/clear.png',
+    '/maaaiiio.github.io/images/cloudy-scattered-showers.png',
+    '/maaaiiio.github.io/images/cloudy.png',
+    '/maaaiiio.github.io/images/fog.png',
+    '/maaaiiio.github.io/images/ic_add_white_24px.svg',
+    '/maaaiiio.github.io/images/ic_refresh_white_24px.svg',
+    '/maaaiiio.github.io/images/partly-cloudy.png',
+    '/maaaiiio.github.io/images/rain.png',
+    '/maaaiiio.github.io/images/scattered-showers.png',
+    '/maaaiiio.github.io/images/sleet.png',
+    '/maaaiiio.github.io/images/snow.png',
+    '/maaaiiio.github.io/images/thunderstorm.png',
+    '/maaaiiio.github.io/images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
